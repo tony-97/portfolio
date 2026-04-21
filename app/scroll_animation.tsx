2bae1adcebde66b1dc4f5d71069d6fa530332cac
tony-97 <taaffaat_1@hotmail.com>
2026-04-20 20:50:20 -0500

    add style prop to ScrollAnimation component for enhanced customization

--- app/scroll_animation.tsx
+++ app/scroll_animation.tsx
@@ -1,23 +1,26 @@
 "use client";
 
 import * as motion from "motion/react-client";
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 
 //transition-all duration-1000 transform ${isVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"}
 
 export default function ScrollAnimation({
   ref,
   id,
+  style,
   children,
 }: {
   ref?: React.Ref<HTMLElement>;
   id?: string;
+  style?: CSSProperties;
   children?: ReactNode;
 }) {
   return (
     <motion.section
       id={id}
       ref={ref}
+      style={style}
       initial={{ opacity: 0, y: 40 }}
       whileInView={{
         opacity: 1,
