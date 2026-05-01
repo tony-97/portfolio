1da73dc73a7513a30a24087f6245d204ecc42a08
tony-97 <taaffaat_1@hotmail.com>
2026-04-30 23:43:01 -0500

    disable scroll smooth when navigating

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
+      data-scroll-behavior="smooth"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
