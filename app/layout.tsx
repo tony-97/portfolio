98974317dfe681604afc999b35bb5e2d3a6ab1b2
tony-97 <taaffaat_1@hotmail.com>
2026-05-02 10:15:05 -0500

    disable smooth scrolling during route transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
+      data-scroll-behavior="smooth"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
