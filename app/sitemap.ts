aac794a2ebf3f5e08e40438e99854a6e9da2d589
tony-97 <taaffaat_1@hotmail.com>
2026-05-12 19:48:57 -0500

    added sitemaps file

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { listProjects } from "@/lib/api";
+import { baseURL } from "@/resources/config";
+import { MetadataRoute } from "next";
+
+export async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projects = await listProjects();
+  return [
+    {
+      url: baseURL,
+      lastModified: new Date("2026-05-09"),
+      priority: 1.0,
+    },
+    {
+      url: `${baseURL}/projects`,
+      lastModified: new Date("2026-05-09"),
+      priority: 0.8,
+    },
+    ...projects.map(
+      ({ slug, metadata: { lastModified } }): MetadataRoute.Sitemap[number] => {
+        return {
+          url: `${baseURL}/projects/${slug}`,
+          lastModified,
+          priority: 0.7,
+        };
+      },
+    ),
+  ];
+}
