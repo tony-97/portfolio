40d75a7597bafbd81e480af98ad2c0a0762af558
tony-97 <taaffaat_1@hotmail.com>
2026-05-25 12:08:50 -0500

    fix extension

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import { baseURL } from "@/resources/config";
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
