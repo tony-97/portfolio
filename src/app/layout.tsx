e1e17b05db1a1bcaa27e450e87f6ffe62e41d4e5
tony-97 <taaffaat_1@hotmail.com>
2026-05-28 18:53:49 -0500

    cleanup styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col bg-background text-foreground font-sans">
+      <body className="min-h-full flex flex-col bg-background text-foreground">
         <ThemeProvider attribute={"class"}>
           <LandingPageProvider>
             <Navigation
